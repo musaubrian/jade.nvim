f143447256f99144fb2b3da895b30db57c4b8646
musaubrian <musaubrian15@gmail.com>
2024-02-24 21:49:04 +0300

    refactor: remove bluw statusline bg

--- lua/jade.lua
+++ lua/jade.lua
@@ -55,7 +55,6 @@ Group.new('CursorLineNr', c.yellow, nil, s.none)
 Group.new('ColorCol', nil, c.light_gray:dark(), s.none)
 Group.new('CursorCol', nil, c.lighter_gray:dark(), s.none)
 Group.new('Builtin', c.turquoise, c.none, s.none)
-Group.new('StatusLine', c.black:light(), c.blue, s.none)
 
 
 Group.link('EndOfBuffer', g.NonText)
@@ -95,4 +94,3 @@ Group.link('Whitespace', g.NonText)
 Group.link('NvimInternalError', g.Error)
 Group.link('FloatBorder', g.NonText)
 Group.link("Include", g.Noise) --imports and stuff
-Group.link("Statusline", g.StatusLine)
